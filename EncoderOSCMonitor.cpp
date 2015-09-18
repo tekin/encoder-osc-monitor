ab32c634ac85e2090324c12e5b0e4d2a59af9ac6
Tekin Suleyman <tekin@tekin.co.uk>
2015-09-18 16:12:48 +0100

    Add rotational speed reporting

--- EncoderOSCMonitor.cpp
+++ EncoderOSCMonitor.cpp
@@ -2,15 +2,32 @@
 
 #define RECEIVER_IP "192.168.1.4"
 #define RECEIVER_PORT 5678
+#define STEPS_PER_REVOLUTION 2500
 
 EncoderOSCMonitor::EncoderOSCMonitor(EthernetUDP &u, uint8_t pin1, uint8_t pin2)
   : udpConnection(u),
     encoder(Encoder(pin1, pin2)),
-    current_position(0) {}
+    current_position(0),
+    update_time(millis()) {}
 
 int32_t EncoderOSCMonitor::readPosition() {
   current_position = encoder.read();
   return(current_position);
+}
+
+float EncoderOSCMonitor::readSpeed() {
+  int32_t old_position        = current_position;
+  int32_t position_difference = readPosition() - old_position;
+  int32_t time_difference     = millis() - update_time;
+  float   revolutions         = (float) position_difference / STEPS_PER_REVOLUTION;
+  float speed = (float) revolutions/time_difference * 1000;
+
+  update_time = millis();
+  return(speed);
+}
+
+void EncoderOSCMonitor::reportSpeed(char* address) {
+  sendMessage(address, readSpeed());
 }
 
 void EncoderOSCMonitor::reportPosition(char* address) {
--- EncoderOSCMonitor.h
+++ EncoderOSCMonitor.h
@@ -8,10 +8,13 @@
   class EncoderOSCMonitor {
   public:
     EncoderOSCMonitor(EthernetUDP &u, uint8_t pin1, uint8_t pin2);
-    int32_t readPosition();
-    void reportPosition(char *address);
+    int32_t readPosition();             // returns the current position of the encoder
+    float readSpeed();                  // returns the current rotations per second of the encoder
+    void reportPosition(char *address); // sends the current position via UDP
+    void reportSpeed(char *address);    // sends the current speed via UDP
   private:
     int32_t current_position;
+    long update_time;
     EthernetUDP &udpConnection;
     Encoder encoder;
     void sendMessage(char *address, float value);
